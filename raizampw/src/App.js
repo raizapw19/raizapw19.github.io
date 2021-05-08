e3c426658270ce1b3b18905347d02ea496ea1715
rmp9 <57851775+raizapw19@users.noreply.github.com>
2021-05-07 20:04:51 -0700

    Initial Commit

---
+++ raizampw/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar.js";
+import Header from "./components/Header.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ raizampw/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <p>
+                    <h1>RAIZA MABBA PARAGGUA-WANA</h1>
+                    <p> Hi, I'm Raiza.</p>
+                </p>  
+            </div>
+        </div>
+    )
+}
+
+//uninstall typed
+export default Header
